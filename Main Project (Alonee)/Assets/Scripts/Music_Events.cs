2c43de552a0f1aac5e848fac7874d053301e9316
AXG Corporation <aakrossh7@gmail.com>
2021-01-25 22:44:00 -0800

    V 3.1 experimenting with music

---
+++ Main Project (Alonee)/Assets/Scripts/Music_Events.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Events : MonoBehaviour
+{
+    public AudioSource horrorAudio;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("horror"))
+        {
+            Debug.Log("Playing Horror sound");
+            horrorAudio.Play();
+        }
+    }
+}
